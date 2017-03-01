d6d01d3ff7769180d8edcbac47de0910042d0fb8
Jesse Shipley <jesse.radack@gmail.com>
2017-03-01 16:28:15 -0700

    add basic forms container to utilize login/logout

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import { render } from 'react-dom';
 import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import Main from './components/Main';
 import Home from './components/Home';
-import './index.css'
+import PromptContainer from './containers/PromptContainer'
+import './index.css';
 
 render((
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
       <IndexRoute component={Home}/>
+      <Route path='login' component={PromptContainer}/>
+      <Route path='signUp/:email' component={PromptContainer}/>
     </Route>
   </Router>
 ), document.getElementById('root'))
